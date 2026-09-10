0bbce71aa63ecdf3462334bf2703d3240e44ed75
John7925 <inventariodeactivofijo@gmail.com>
2026-09-10 11:18:22 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'consulta-af-shell-v0.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).catch(() => caches.match('./index.html'))
+    )
+  );
+});
